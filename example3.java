c77236ffa663bf6eaf08e9129726d972a36d34d0
Aayush Kantak <120012051+aysh01@users.noreply.github.com>
2023-09-12 16:38:10 +0530

    Create example3.java

---
+++ example3.java
@@ -0,0 +1,9 @@
+//Returning a Current Instance
+public class example3 {
+	private int value;
+	
+	public example3 set(int value) {
+		this.value = value;
+		return this; //Return current instance of a class
+	}
+}
