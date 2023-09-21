eeac8c82e928f22ea366887bdc1e35e31fbaecfa
Aayush Kantak <120012051+aysh01@users.noreply.github.com>
2023-09-21 15:57:43 +0530

    Update example.java

--- JavaScript_/example.java
+++ JavaScript_/example.java
@@ -22,8 +22,14 @@ const x=new example();
 x.append(2); //Appending values inside array
 x.append(10); //Appending values inside array
 x.append(3); //Appending values inside array
+x.display(); //Here, Display method is used once to display the array [2,10,3]
 
-x.remove();
-x.remove();
-x.display(); //Display method is used to display the array
+x.remove(); //Remove() function is used to pop the last element from the array.
+x.display(); //display()
+
+
+//We, can use Remove() function multiple times to pop the last element from the array.
+// x.remove(); 
+// x.display(); 
+
 
