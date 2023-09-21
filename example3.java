f3b3e0a0cd249ba6986c25090f47a20e3fd613fc
Aayush Kantak <120012051+aysh01@users.noreply.github.com>
2023-09-21 16:03:52 +0530

    Delete example3.java

--- example3.java
+++
@@ -1,9 +0,0 @@
-//Returning a Current Instance
-public class example3 {
-	private int value;
-	
-	public example3 set(int value) {
-		this.value = value;
-		return this; //Return current instance of a class
-	}
-}
