37817fa39c9d6f23a83623d606acbe856c86e410
Aayush Kantak <120012051+aysh01@users.noreply.github.com>
2023-10-27 14:44:52 +0530

    Create example.java

---
+++ Java.util.Arrays/example.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class example{
+    public static void main(String [] args){
+        int a[] = new int[5];
+        java.util.Arrays.fill(a,1);
+        for(int i=0;i<=a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
